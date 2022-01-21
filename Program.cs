5dd123a9334392f6d066eaf015cd88f99b08ad99
Jorge Tinajero <jotinajero07@hotmail.com>
2022-01-21 11:06:18 -0600

    fixed some issues

--- Program.cs
+++ Program.cs
@@ -5,27 +5,31 @@ namespace C_.net
     class Program
     {
         public static String MaxOccur(String word){
-            int [] count = new int[256];
+            int [] count = new int[512];
             int max = 0; 
             String result = ""; 
 
+            Console.WriteLine("before first for");
             for(int i =0; i<word.Length; i++){
                 count[word[i]]++;
             }
-
+            Console.WriteLine("after first for");
             for(int i =0; i<word.Length; i++){
                 // Console.WriteLine(word[i]);
-                if(max < count[word[i]] ){
+                if(max < count[word[i]] && count[word[i]] >1){
                     max = count[word[i]];
                     result = word[i].ToString();
                     // Console.WriteLine("inside if" + word[i]);
                 }
-                if(max == count[word[i]]){
+                if(max == count[word[i]] && count[word[i]] > 1){
                     result += word[i].ToString();
                     // Console.WriteLine(word[i]);
                 }
-
             }
+            if(max == 0){
+                result = "No Repetition";
+            }
+            Console.WriteLine("before sending result");
             return result;
         }
         static void Main(string[] args)
@@ -35,30 +39,35 @@ namespace C_.net
             Boolean running = true;
             String returned = "";
             while(running){
-                Console.WriteLine("Please type a word");
+                Console.WriteLine("Please type a word:");
                 word = Console.ReadLine();
+                // word = word.Replace(" ", "");
                 returned = MaxOccur(word);
-                char [] forWord = new char [returned.Length];
-                
-                for(int i =0; i<word.Length; i++){
-                    if(returned[i] != forWord[i]){
-                        forWord[i] = returned[i];
-                    }
-                }
+                if(!returned.Equals("No Repetition")){
+                    char [] forWord = new char [1024];
 
-                for(int i =0; i < forWord.Length; i++){
-                    Boolean isDuplicate = false;
-                    for(int j = 0; j<i; j++){
-                        if(forWord[i] == forWord[j]){
-                            isDuplicate = true;
-                            break;
+                    for(int i =0; i<word.Length; i++){
+                        if(returned[i] != forWord[i]){
+                            forWord[i] = returned[i];
                         }
                     }
-                     if(!isDuplicate){  
-                        Console.Write(forWord[i]);    
-                    }  
-                }
 
+                    for(int i =0; i < forWord.Length; i++){
+                        Boolean isDuplicate = false;
+                        for(int j = 0; j<i; j++){
+                            if(forWord[i] == forWord[j]){
+                                isDuplicate = true;
+                                break;
+                            }
+                        }
+                        if(!isDuplicate){  
+                            Console.Write(forWord[i] + " ,");    
+                        }  
+                    }
+                }
+                else{
+                    Console.Write(returned);
+                }
             
                 Console.WriteLine("\nWould you like to exit? Press x");
                 String resp = Console.ReadLine();
